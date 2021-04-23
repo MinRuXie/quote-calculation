4e0216eda11d9afe84e9f42dee4e66ad6d2171a6
Joy <a556987@gmail.com>
2021-04-23 20:21:44 +0800

    update health rate

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var vm = new Vue({
         tax_included_quotation: 0, // 含稅報價(支領金額)
         tax_exclude_quotation: 0, // 不含稅報價(支領金額) 
         toggle: true, // 是否勾選健保費
-        health_insurance_rate: 0.0191, // 健保費率
+        health_insurance_rate: 0.0211, // 健保費率 2021 2.11%
         health_insurance: 0, // 健保費
         income_tax_rate: 0.1, // 所得稅率(低於兩萬不扣)
         income_tax: 0, // 所得稅
@@ -18,21 +18,21 @@ var vm = new Vue({
         /*
             支領金額 X
             代扣所得稅(10%) 0.1
-            健保補充保費(1.91%) 0.0191
+            健保補充保費(2.11%) 0.0211
             含稅價 0.8809X
 
             支領金額 -健保 -所得 = 含稅價
-            X - 0.1X - 0.0191X = 0.8809X
+            X - 0.1X - 0.0211X = 0.8809X
 
             倒推公式
             支領金額 -健保 -所得 = 含稅價
-            X/0.8809 - 0.1X/0.8809 - 0.0191X/0.8809 = X
+            X/0.8809 - 0.1X/0.8809 - 0.0211X/0.8809 = X
         */
 
         // 健保費
         getHealthInsurance: function () {
             if (this.toggle) {
-                this.health_insurance_rate = 0.0191;
+                this.health_insurance_rate = 0.0211;
                 this.health_insurance = Math.floor((this.health_insurance_rate * this.final_actual_collar)/(1 - this.health_insurance_rate - this.income_tax_rate));
             } else {
                 this.health_insurance_rate = 0;
